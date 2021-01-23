9dc044c76556eefd8b03fa131ff110cf839d30ce
Andrew Godwin <andrew@aeracode.org>
2021-01-22 23:01:10 -0700

    Improvements to void-filling and add max-z limit

--- landcarve/commands/lasdem.py
+++ landcarve/commands/lasdem.py
@@ -1,3 +1,4 @@
+import functools
 import sys
 
 import click
@@ -19,9 +20,16 @@ from landcarve.utils.io import array_to_raster
     type=int,
     help="Max distance to try and fill voids from",
 )
+@click.option(
+    "-z",
+    "--z-limit",
+    default=4000,
+    type=int,
+    help="Maximum elevation to trust; discard anything above",
+)
 @click.argument("input_paths", nargs=-1)
 @click.argument("output_path")
-def lasdem(input_paths, output_path, snap, void_distance):
+def lasdem(input_paths, output_path, snap, void_distance, z_limit):
     """
     Turns a raw .las or .laz file into a DEM
     """
@@ -30,28 +38,32 @@ def lasdem(input_paths, output_path, snap, void_distance):
         click.echo("You must provide at least one input file")
         sys.exit(1)
 
-    # Open the LAS files
-    las_files = [laspy.file.File(input_path, mode="r") for input_path in input_paths]
-
     # Work out the projection
     projection = None
-    for las in las_files:
-        las_projection = "unknown"
-        for vlr in las.header.vlrs:
-            if vlr.record_id == 34735:  # GeoTIFF tag format
-                num_tags = vlr.parsed_body[3]
-                for i in range(num_tags):
-                    key_id = vlr.parsed_body[4 + (i * 4)]
-                    offset = vlr.parsed_body[7 + (i * 4)]
-                    if key_id == 3072:
-                        srs = osr.SpatialReference()
-                        srs.ImportFromEPSG(offset)
-                        las_projection = srs.ExportToWkt()
-                        break
-        if projection is None or projection == las_projection:
-            projection = las_projection
-        else:
-            click.echo(f"Mismatched projections - {las} does not match the first file")
+    las_files = []
+    with click.progressbar(length=len(input_paths), label="Opening files") as bar:
+        for input_path in input_paths:
+            bar.update(1)
+            las = laspy.file.File(input_path, mode="r")
+            las_files.append(las)
+            las_projection = "unknown"
+            for vlr in las.header.vlrs:
+                if vlr.record_id == 34735:  # GeoTIFF tag format
+                    num_tags = vlr.parsed_body[3]
+                    for i in range(num_tags):
+                        key_id = vlr.parsed_body[4 + (i * 4)]
+                        offset = vlr.parsed_body[7 + (i * 4)]
+                        if key_id == 3072:
+                            srs = osr.SpatialReference()
+                            srs.ImportFromEPSG(offset)
+                            las_projection = srs.ExportToWkt()
+                            break
+            if projection is None or projection == las_projection:
+                projection = las_projection
+            else:
+                click.echo(
+                    f"Mismatched projections - {las} does not match the first file"
+                )
 
     # Calculate the bounds of all files (initial values are a bit stupid)
     click.echo(f"{len(input_paths)} file(s) provided")
@@ -65,8 +77,8 @@ def lasdem(input_paths, output_path, snap, void_distance):
         max_y = max(max_y, las.y.max())
 
     # Calculate the size of the final array
-    x_index = lambda v: int((v - min_x) // snap)
-    y_index = lambda v: int((v - min_y) // snap)
+    x_index = functools.lru_cache(maxsize=10240)(lambda v: int((v - min_x) // snap))
+    y_index = functools.lru_cache(maxsize=10240)(lambda v: int((v - min_y) // snap))
     x_size = x_index(max_x) + 1
     y_size = y_index(max_y) + 1
 
@@ -79,34 +91,39 @@ def lasdem(input_paths, output_path, snap, void_distance):
 
     # For each point, bucket it into the right array coord
     with click.progressbar(length=num_points, label="Thinning") as bar:
+        n = 0
         for las in las_files:
             points = numpy.vstack((las.x, las.y, las.z)).T
-            for x, y, z in points:
-                bar.update(1)
-                new_x = x_index(x)
-                new_y = y_index(y)
-                arr[new_y][new_x] = max(arr[new_y][new_x], z)
+            for i, (x, y, z) in enumerate(points):
+                if z <= z_limit:
+                    new_x = x_index(x)
+                    new_y = y_index(y)
+                    arr[new_y][new_x] = max(arr[new_y][new_x], z)
+                n += 1
+                if n > 1000:
+                    bar.update(1000)
+                    n = 0
+
+    # Scan for all voids in the array
+    voids = set()
+    with click.progressbar(length=arr.shape[0], label="Discovering voids") as bar:
+        for y in range(arr.shape[0]):
+            bar.update(1)
+            for x in range(arr.shape[1]):
+                if arr[y][x] <= NODATA:
+                    voids.add((x, y))
 
     # Fill any voids by finding their nearest neighbour
-    void_fills = []
-    with click.progressbar(length=x_size * y_size, label="Filling voids") as bar:
-        for x in range(arr.shape[0]):
-            for y in range(arr.shape[1]):
-                bar.update(1)
-                if arr[x][y] <= NODATA:
-                    void_fills.append(
-                        (
-                            x,
-                            y,
-                            find_nearest_neighbour(arr, x, y, distance=void_distance),
-                        )
-                    )
-
-    # We write void fills back separately so they don't interfere with each other
-    for x, y, z in void_fills:
-        if z > 2000:
-            click.echo(x, y, z)
-        arr[x][y] = z
+    num_voids = len(voids)
+    with click.progressbar(length=void_distance, label="Filling voids") as bar:
+        for _ in range(void_distance):
+            bar.update(1)
+            for x, y in list(voids):
+                z = find_nearest_neighbour(arr, x, y)
+                if z > NODATA:
+                    arr[y][x] = z
+                    voids.remove((x, y))
+    click.echo("Removed %s / %s voids" % (num_voids - len(voids), num_voids))
 
     # Write out a TIF
     array_to_raster(
@@ -117,29 +134,28 @@ def lasdem(input_paths, output_path, snap, void_distance):
     )
 
 
-def find_nearest_neighbour(arr, x, y, distance=10):
+def find_nearest_neighbour(arr, x, y):
     """
     Finds the nearest non-NODATA value to x, y
     """
-    for d in range(distance):
-        for dx, dy in [
-            (1, 0),
-            (0, 1),
-            (-1, 0),
-            (0, -1),
-            (1, 1),
-            (1, -1),
-            (-1, -1),
-            (-1, 1),
-        ]:
-            new_x = x + (dx * d)
-            new_y = y + (dy * d)
-            if (
-                new_x >= 0
-                and new_x < arr.shape[0]
-                and new_y >= 0
-                and new_y < arr.shape[1]
-                and arr[new_x][new_y] > NODATA
-            ):
-                return arr[new_x][new_y]
+    for dx, dy in [
+        (1, 0),
+        (0, 1),
+        (-1, 0),
+        (0, -1),
+        (1, 1),
+        (1, -1),
+        (-1, -1),
+        (-1, 1),
+    ]:
+        new_x = x + dx
+        new_y = y + dy
+        if (
+            new_x >= 0
+            and new_x < arr.shape[1]
+            and new_y >= 0
+            and new_y < arr.shape[0]
+            and arr[new_y][new_x] > NODATA
+        ):
+            return arr[new_y][new_x]
     return NODATA
